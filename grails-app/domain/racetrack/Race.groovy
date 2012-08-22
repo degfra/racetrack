e0dd8c1d810a7065a361ec7901c40def5853e600
degfra <degfra2@gmail.com>
2012-08-22 18:35:53 +0200

    Initial Commit

---
+++ grails-app/domain/racetrack/Race.groovy
@@ -0,0 +1,30 @@
+package racetrack
+
+class Race {
+	
+	static constraints = {
+		name(blank:false, maxSize:50, notEqual:"Buster", unique:true) //blank:false,
+		startDate()
+		city()
+		state(inList: ["GA", "NC", "SC", "VA"])
+		distance(min:0.0) //min:0.0, range:0..10
+		cost(min:0.0, max:100.0)
+		maxRunners(min:0, max:100000)
+		
+		startDate(validator: {return (it > new Date())})
+	}
+	
+	String name
+	Date startDate
+	String city
+	String state
+	BigDecimal distance
+	BigDecimal cost
+	Integer maxRunners = 100000
+	
+	BigDecimal inMiles(){
+		return distance * 0.6214
+	}
+
+    
+}
