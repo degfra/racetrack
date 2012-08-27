092acb96c857df42c2a1050a16293247f6260624
degfra <degfra2@gmail.com>
2012-08-27 16:50:01 +0200

    Mysql Data Source

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,33 +1,43 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
+    /*driverClassName = "org.h2.Driver"
     username = "sa"
-    password = ""
+    password = "" */
+	driverClassName = "com.mysql.jdbc.Driver"
+	username = "grails"
+	password = "server"
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
+    //cache.use_query_cache = false
+	cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	//cache.provider_class = 'com.opensymphony.oscache.hibernate.OSCacheProvider'
+	
 }
 // environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = "jdbc:mysql://localhost:3306/racetrack_dev? autoreconnect=true"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            //url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = "jdbc:mysql://localhost:3306/racetrack_test? autoreconnect=true"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
+            //url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			url = "jdbc:mysql://localhost:3306/racetrack_prod? autoreconnect=true"
+            
+			/*pooled = true
             properties {
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
@@ -36,8 +46,8 @@ environments {
                testOnBorrow=true
                testWhileIdle=true
                testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+               validationQuery="SELECT 1" 
+            } */
         }
     }
 }
