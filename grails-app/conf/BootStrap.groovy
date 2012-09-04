5dcbcd6981cbc0f2a90a332cd7782f22a1357d30
degfra <degfra2@gmail.com>
2012-09-04 18:35:26 +0200

    Prod Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,70 +6,85 @@ import grails.util.Environment
 
 class BootStrap {
 
-    def init = { servletContext ->
-	  switch(Environment.current){
-	   case Environment.DEVELOPMENT:
-	   
-	   	def admin = new User(login:"admin", password:"wordpass", role:"admin")
-		   
-		admin.save()
-		if(admin.hasErrors()){
-			println admin.errors
+	def init = { servletContext ->
+		switch(Environment.current){
+			case Environment.DEVELOPMENT:
+
+				def admin = new User(login:"admin", password:"wordpass", role:"admin")
+
+				admin.save()
+				if(admin.hasErrors()){
+					println admin.errors
+				}
+
+				def jdoe = new User(login:"jdoe", password:"password", role:"user")
+
+				jdoe.save()
+				if(jdoe.hasErrors()){
+					println jdoe.errors
+				}
+
+				def jane = new Runner(
+						firstName:"Jane",
+						lastName:"Doe",
+						dateOfBirth:(new Date() - 365*30),
+						gender:"F",
+						address:"123 Main St",
+						city:"Goose",
+						state: "NC",
+						zipcode:"12345",
+						email:"jane@whereever.com"
+						)
+
+				jane.save()
+				if(jane.hasErrors()){
+					println jane.errors
+				}
+
+				def trot = new Race(
+						name:"Turkey Trot",
+						startDate:(new Date()+ 90),
+						city:"Duck",
+						state:"NC",
+						distance:5.0,
+						cost:20.0,
+						maxRunners:350
+						)
+				trot.save()
+				if(trot.hasErrors()){
+					println trot.errors
+				}
+
+				def reg = new Registration(
+						paid:false,
+						runner:jane,
+						race:trot
+						)
+				reg.save()
+				if(reg.hasErrors()){
+					println reg.errors
+				}
+
+				break
+
+			case Environment.PRODUCTION:
+
+				def admin = new User(login:"admin", password:"wordpass", role:"admin")
+
+				admin.save()
+				if(admin.hasErrors()){
+					println admin.errors
+				}
+
+				def jdoe = new User(login:"jdoe", password:"password", role:"user")
+
+				jdoe.save()
+				if(jdoe.hasErrors()){
+					println jdoe.errors
+				}
+
+				break
 		}
-		
-		def jdoe = new User(login:"jdoe", password:"password", role:"user")
-		
-		jdoe.save()
-		if(jdoe.hasErrors()){
-			println jdoe.errors
-		} 
-		
-		def jane = new Runner(
-			firstName:"Jane", 
-			lastName:"Doe",
-			dateOfBirth:(new Date() - 365*30),
-			gender:"F",
-			address:"123 Main St",
-			city:"Goose",
-			state: "NC",
-			zipcode:"12345",
-			email:"jane@whereever.com"
-			)
-		
-		jane.save()
-		if(jane.hasErrors()){
-			println jane.errors
-		}
-		
-		def trot = new Race(
-			name:"Turkey Trot",
-			startDate:(new Date()+ 90),
-			city:"Duck",
-			state:"NC",
-			distance:5.0,
-			cost:20.0,
-			maxRunners:350
-			)
-		trot.save()
-		if(trot.hasErrors()){
-			println trot.errors
-		}
-		
-		def reg = new Registration(
-			paid:false,
-			runner:jane,
-			race:trot
-			)
-		reg.save()
-		if(reg.hasErrors()){
-			println reg.errors
-		}
-		
-		break
-		
-	   case Environment.PRODUCTION: break 
-		
-	  }
-    }
-    def destroy = { }
+	}
+	def destroy = { }
 }
